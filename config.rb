bbb7a2b5927bf331d161018748a2354c45bcfe04
Steve Paul <stevepaul325@gmail.com>
2021-02-20 00:45:30 +0530

    Update design

--- config.rb
+++ config.rb
@@ -5,6 +5,9 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :livereload
+activate :imageoptim
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
